1d9080736fb47c7c1b790fc8af792cf412d93145
MikhelRhyz <mikhelrhyz@gmail.com>
2025-01-14 17:05:26 +0800

    add new lessons

---
+++ 6 - JavaScript/1-variables.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+// This is a single-line comment.
+/* 
+This is a
+multi-line
+comment.
+*/
+// Variable: allows us to store data.
+// let - mutable/changeable
+// const
+// var
+let firstName = "John";
+console.log("Firstname:", firstName);
+firstName = "Jane";
+console.log("Firstname:", firstName);
+const birthday = "02-14-2000";
+console.log("Birthday:", birthday);
+// birthday = "03-14-1999" This will return a TypeError.
+// Variable: allows us to store data.
+// let - mutable/changeable - block-scope
+// const - immutable/unchangeable
+// var - mutable/changeable - global-scope
---
+++ 6 - JavaScript/app.js
@@ -0,0 +1,19 @@
+// 1. Function Definition
+function greet(){
+    console.log("Hello, welcome to the world of functions!");
+    console.log("Let's go!!")
+}
+// Calling a function (invoking a function)
+greet()
+greet()
+greet()
+
+// 2. Function definition with parameters.
+// parameters - placeholder
+// arguments - values passed to the functions parameters.
+function sum(num1, num2){
+    return num1 + num2
+}
+console.log("Sum of 10 and 20:", sum(10, 20))
+console.log("Sum of 30 and 50:", sum(30, 50))
+
